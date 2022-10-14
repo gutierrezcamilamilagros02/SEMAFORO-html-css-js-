bc6030d1e8b459255465c0000b268486bed8a37c
gutierrezcamilamilagros02 <115835438+gutierrezcamilamilagros02@users.noreply.github.com>
2022-10-14 16:51:19 -0300

    Add files via upload

---
+++ semaforo/script.js
@@ -0,0 +1,16 @@
+
+
+const $lucesDelCirculo = document.querySelectorAll('.luces-circulo');
+let contadorDeLuz = 0;
+
+let mostrarLuz = () => {
+    $lucesDelCirculo[contadorDeLuz].className = 'luces-circulo';
+    contadorDeLuz++;
+
+    if(contadorDeLuz > 2) contadorDeLuz = 0;
+
+    const luzActual = $lucesDelCirculo[contadorDeLuz];
+    luzActual.classList.add(luzActual.getAttribute('color'))
+}
+
+setInterval(mostrarLuz,1000)
